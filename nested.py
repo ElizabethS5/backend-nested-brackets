d3315d4b62f2d1129411c19dfd5e2ba771205e93
Elizabeth Scheidt <escheidt5@gmail.com>
2019-08-12 21:58:41 -0400

    refactor test_bracket_list function

--- nested.py
+++ nested.py
@@ -33,57 +33,34 @@ def string_to_list(string):
 def test_bracket_list(bracket_list):
     """If bracket_list passes return 'Yes' else return 'No' and failing position"""
     copy = bracket_list[:]
-    greater_less = [0]
-    curly = [0]
-    square = [0]
-    paren = [0]
-    aster = [0]
-    combo = [greater_less[-1], curly[-1],
-             square[-1], paren[-1], aster[-1]]
+    all_list = []
+    rights = '>}])'
+    lefts = '<{[('
     position = 1
-    while position <= len(bracket_list):
-        if copy[0] == '>':
-            if len(greater_less) == 1 or greater_less[-1] < max(combo):
-                return f'NO {position}\n'
+    while copy:
+        if copy[0][-1] in rights:
+            if len(all_list) == 0:
+                return f"NO {position}\n"
+            elif copy[0] == '>' and all_list[-1] == '<':
+                all_list.pop()
+            elif copy[0] == ']' and all_list[-1] == '[':
+                all_list.pop()
+            elif copy[0] == '}' and all_list[-1] == '{':
+                all_list.pop()
+            elif copy[0] == ')' and all_list[-1] == '(':
+                all_list.pop()
+            elif copy[0] == '*)' and all_list[-1] == '(*':
+                all_list.pop()
             else:
-                greater_less.pop()
-        elif copy[0] == '}':
-            if len(curly) == 1 or curly[-1] < max(combo):
-                return f'NO {position}\n'
+                return f"NO {position}\n"
+        elif copy[0][0] in lefts:
+            if len(copy) == 1:
+                return f"NO {position}"
             else:
-                curly.pop()
-        elif copy[0] == ']':
-            if len(square) == 1 or square[-1] < max(combo):
-                return f'NO {position}\n'
-            else:
-                square.pop()
-        elif copy[0] == ')':
-            if len(paren) == 1 or paren[-1] < max(combo):
-                return f'NO {position}\n'
-            else:
-                paren.pop()
-        elif copy[0] == '*)':
-            if len(aster) == 1 or aster[-1] < max(combo):
-                return f'NO {position}\n'
-            else:
-                aster.pop()
-        elif len(copy) == 1 and copy[0][0] in '<[{(':
-            return f'NO {position}\n'
-        elif copy[0] == '<':
-            greater_less.append(position)
-        elif copy[0] == '{':
-            curly.append(position)
-        elif copy[0] == '[':
-            square.append(position)
-        elif copy[0] == '(':
-            paren.append(position)
-        elif copy[0] == '(*':
-            aster.append(position)
+                all_list.append(copy[0])
         position += 1
         copy.pop(0)
-        combo = [greater_less[-1], curly[-1],
-                 square[-1], paren[-1], aster[-1]]
-    if max(combo) != 0:
+    if len(all_list) != 0:
         return f'NO {position}\n'
     else:
         return 'YES\n'
